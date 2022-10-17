4e76895545714d2f78980654339e154b945ab6ae
adarshgithubit <104212058+adarshgithubit@users.noreply.github.com>
2022-10-18 00:31:23 +0530

    added

---
+++ src/Todo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./App.css"
+
+const Todo = () => {
+  return (
+    <>
+    <div className='container'>
+        <div className='box'>
+        
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Todo
